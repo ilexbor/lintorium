8033324e411fce320853e9f437aba32570b0e3b9
ilexbor <this.is@ilexbor.dev>
2025-11-29 15:40:39 +0300

    Improved version_checker script

--- cli/bin/version_switcher.dart
+++ cli/bin/version_switcher.dart
@@ -1,25 +1,36 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:version/version.dart';
 import 'package:yaml/yaml.dart';
 
-void main() {
+Future<void> main() async {
   try {
     final lintoriumVersion = _getLintoriumVersionFromPubspecFile();
 
-    final (minDartSdkVersion: minDartSdkVersion, maxDartSdkVersion: maxDartSdkVersion) = _getDartSdkVersions(
+    stdout.writeln('Lintorium version: $lintoriumVersion');
+
+    final (:minDartSdkVersion, :maxDartSdkVersion) = _getDartSdkVersions(
       lintoriumVersion,
     );
 
-    stdout.writeln('Lintorium version: $lintoriumVersion');
     stdout.writeln('Dart SDK min version: $minDartSdkVersion');
     stdout.writeln('Dart SDK max version: $maxDartSdkVersion');
 
-    _updateAnalysisOptionsFile(minDartSdkVersion.toString());
+    _switchDartVersion(minDartSdkVersion, maxDartSdkVersion);
 
-    _updatePubspecFile(File('../pubspec.yaml'), minDartSdkVersion, maxDartSdkVersion);
-    _updatePubspecFile(File('../example/pubspec.yaml'), minDartSdkVersion, maxDartSdkVersion);
+    final flutterSdkVersion = _getFlutterSdkVersion(minDartSdkVersion);
 
+    stdout.writeln('Flutter SDK version: $flutterSdkVersion');
+
+    await _switchFlutterVersion(flutterSdkVersion);
+
+    await _runDartVersion();
+    await _runFlutterVersion();
+
+    await _runPubGet();
+
+    stdout.writeln();
     stdout.writeln('✅ Success');
   } on Exception catch (e) {
     stdout.writeln('⚠️ Error: $e');
@@ -27,14 +38,18 @@ void main() {
   }
 }
 
-Version _getLintoriumVersionFromPubspecFile() {
-  final file = File('../pubspec.yaml');
+String _getWorkspaceDir() {
+  return File(Platform.script.toFilePath()).absolute.parent.parent.parent.path;
+}
 
-  if (!file.existsSync()) {
-    throw Exception('File `${file.absolute.path}` not found');
+Version _getLintoriumVersionFromPubspecFile() {
+  final pubspecYamlFile = File('${_getWorkspaceDir()}/pubspec.yaml');
+
+  if (!pubspecYamlFile.existsSync()) {
+    throw Exception('File `${pubspecYamlFile.absolute.path}` not found');
   }
 
-  final content = file.readAsStringSync();
+  final content = pubspecYamlFile.readAsStringSync();
 
   final yaml = loadYaml(content) as Map;
 
@@ -68,8 +83,38 @@ Version _getLintoriumVersionFromPubspecFile() {
   return (minDartSdkVersion: minDartSdkVersion, maxDartSdkVersion: maxDartSdkVersion);
 }
 
+Version _getFlutterSdkVersion(Version dartSdkVersion) {
+  return switch (dartSdkVersion) {
+    Version(major: 3, minor: 0) => Version(3, 10, 0),
+    Version(major: 3, minor: 1) => Version(3, 13, 0),
+    Version(major: 3, minor: 2) => Version(3, 16, 0),
+    Version(major: 3, minor: 3) => Version(3, 19, 0),
+    Version(major: 3, minor: 4) => Version(3, 22, 0),
+    Version(major: 3, minor: 5) => Version(3, 24, 0),
+    Version(major: 3, minor: 6) => Version(3, 27, 0),
+    Version(major: 3, minor: 7) => Version(3, 29, 0),
+    Version(major: 3, minor: 8) => Version(3, 32, 0),
+    Version(major: 3, minor: 9) => Version(3, 35, 4),
+    Version(major: 3, minor: 10) => Version(3, 38, 2),
+    _ => throw Exception(
+        'No Flutter version mapping found for Dart $dartSdkVersion. '
+        'Please update _getFlutterSdkVersion method.',
+      ),
+  };
+}
+
+void _switchDartVersion(Version minDartSdkVersion, Version maxDartSdkVersion) {
+  _updateAnalysisOptionsFile(minDartSdkVersion.toString());
+  _updatePubspecYamlFile(File('${_getWorkspaceDir()}/pubspec.yaml'), minDartSdkVersion, maxDartSdkVersion);
+  _updatePubspecYamlFile(
+    File('${_getWorkspaceDir()}/example/pubspec.yaml'),
+    minDartSdkVersion,
+    maxDartSdkVersion,
+  );
+}
+
 void _updateAnalysisOptionsFile(String dartVersion) {
-  final file = File('../lib/analysis_options.yaml');
+  final file = File('${_getWorkspaceDir()}/lib/analysis_options.yaml');
 
   if (!file.existsSync()) {
     throw Exception('analysis_options.yaml not found at ${file.path}');
@@ -95,7 +140,7 @@ void _updateAnalysisOptionsFile(String dartVersion) {
   file.writeAsStringSync(newContent);
 }
 
-void _updatePubspecFile(File file, Version minDartSdkVersion, Version maxDartSdkVersion) {
+void _updatePubspecYamlFile(File file, Version minDartSdkVersion, Version maxDartSdkVersion) {
   if (!file.existsSync()) {
     throw Exception('File `${file.absolute.path}` not found');
   }
@@ -109,4 +154,84 @@ void _updatePubspecFile(File file, Version minDartSdkVersion, Version maxDartSdk
   });
 
   file.writeAsStringSync(updatedContent);
+}
+
+Future<void> _switchFlutterVersion(Version flutterSdkVersion) async {
+  final exitCode = await _runProcess(
+    'fvm',
+    ['use', flutterSdkVersion.toString()],
+    _getWorkspaceDir(),
+  );
+
+  if (exitCode != 0) {
+    throw Exception('Failed to switch Flutter version');
+  }
+}
+
+Future<int> _runProcess(
+  String executable,
+  List<String> arguments,
+  String workingDirectory,
+) async {
+  final process = await Process.start(
+    executable,
+    arguments,
+    workingDirectory: workingDirectory,
+  );
+
+  final stdoutFuture = process.stdout.transform(utf8.decoder).forEach((data) {
+    stdout.write(data);
+  });
+
+  final stderrFuture = process.stderr.transform(utf8.decoder).forEach((data) {
+    stderr.write(data);
+  });
+
+  await Future.wait([stdoutFuture, stderrFuture]);
+  return process.exitCode;
+}
+
+Future<void> _runDartVersion() async {
+  stdout.writeln();
+  stdout.writeln('> fvm dart --version');
+
+  await _runProcess(
+    'fvm',
+    ['dart', '--version'],
+    _getWorkspaceDir(),
+  );
+}
+
+Future<void> _runFlutterVersion() async {
+  stdout.writeln();
+  stdout.writeln('> fvm flutter --version');
+
+  await _runProcess(
+    'fvm',
+    ['flutter', '--version'],
+    _getWorkspaceDir(),
+  );
+}
+
+Future<void> _runPubGet() async {
+  final packages = [
+    _getWorkspaceDir(),
+    '${_getWorkspaceDir()}/cli',
+    '${_getWorkspaceDir()}/example',
+  ];
+
+  for (final packageDir in packages) {
+    stdout.writeln();
+    stdout.writeln('> fvm dart pub get (in $packageDir)');
+
+    final exitCode = await _runProcess(
+      'fvm',
+      ['dart', 'pub', 'get'],
+      packageDir,
+    );
+
+    if (exitCode != 0) {
+      throw Exception('Failed to run pub get in $packageDir');
+    }
+  }
 }
